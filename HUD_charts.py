c1a1317fc7ebcb67ebab7e62e8e5026081d672b6
hong2tu <58047668+hong2tu@users.noreply.github.com>
2020-02-22 16:57:28 -0500

    Update HUD_charts.py

--- HUD_charts.py
+++ HUD_charts.py
@@ -1,17 +1,31 @@
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 df = pd.read_excel('Data_Level2_HUD_HUDPrograms_2009.xlsx')
 
+
 race = df['HEAD_RACE_CD'].value_counts(normalize=True) * 100
 
 print(race)
 plt.pie(race, pctdistance=2, autopct='%1.01f%%')
 plt.legend(labels=race.keys(), bbox_to_anchor=(-0.1, 1.),fontsize=8)
 plt.show()
+
+
+
+race = df['pgm_type_edited'].value_counts(normalize=True) * 100
+
+print(race)
+plt.pie(race, pctdistance=2, autopct='%1.01f%%')
+plt.legend(labels=race.keys(), bbox_to_anchor=(-0.1, 1.),fontsize=8)
+plt.show()
+
+
 
 df = pd.read_excel('Data_Level2_HUD_HUDPrograms_2014.xlsx')
 
+
 race = df['HEAD_RACE_CD'].value_counts(normalize=True) * 100
 
 print(race)
@@ -19,9 +33,34 @@ plt.pie(race, pctdistance=2, autopct='%1.01f%%')
 plt.legend(labels=race.keys(), bbox_to_anchor=(-0.1, 1.),fontsize=8)
 plt.show()
 
+
+# In[115]:
+
+
+race = df['pgm_type_edited'].value_counts(normalize=True) * 100
+
+print(race)
+plt.pie(race, pctdistance=2, autopct='%1.01f%%')
+plt.legend(labels=race.keys(), bbox_to_anchor=(-0.1, 1.),fontsize=8)
+plt.show()
+
+
+
+
 df = pd.read_excel('Data_Level2_HUD_HUDPrograms_2018.xlsx')
 
+
+
+
 race = df['HEAD_RACE_CD'].value_counts(normalize=True) * 100
+
+print(race)
+plt.pie(race, pctdistance=2, autopct='%1.01f%%')
+plt.legend(labels=race.keys(), bbox_to_anchor=(-0.1, 1.),fontsize=8)
+plt.show()
+
+
+race = df['pgm_type_edited'].value_counts(normalize=True) * 100
 
 print(race)
 plt.pie(race, pctdistance=2, autopct='%1.01f%%')
