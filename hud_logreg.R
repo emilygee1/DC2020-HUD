b223c8658f4292596148310df24428cfbc95f0f0
Emily Gee <emilygee1@users.noreply.github.com>
2020-02-23 18:04:52 -0500

    logreg model with select predictors

--- hud_logreg.R
+++ hud_logreg.R
@@ -9,8 +9,26 @@ training<-hud[index,]
 test<-hud[-index,]
 
 library(nnet)
-#multinomial logistic regression model with all predictors
-tmodel<-multinom(pgm_type_edited ~ ., data=training)
+#multinomial logistic regression model with chosen predictors
+tmodel<-multinom(pgm_type_edited ~ 
+                   HSHD_MBR_TOTAL_CNT+
+                   TOTAL_DPNDNT_CNT+
+                   TOTAL_ANNL_INCM_AMNT+
+                   HEAD_GNDR_CD+
+                   HEAD_AGE_YR_CNT+
+                   HEAD_ELDLY_INDR+
+                   HEAD_RACE_CD+
+                   HEAD_ETHNCY_CD+
+                   DSBLD_CHLDRN_CNT+
+                   CHLDRN_MBR_CNT+
+                   GROSS_RENT_AMNT+
+                   TOTAL_FMLY_CRBTN_AMNT+
+                   PVRTY_PRCNT+
+                   MNRTY_PRCNT+
+                   BLACK_PRCNT+
+                   HISPANIC_PRCNT+
+                   WHITE_PRCNT
+                 , data=training)
 summary(tmodel)
 
 z <- summary(tmodel)$coefficients/summary(tmodel)$standard.errors
@@ -21,5 +39,8 @@ p
 predicted_scores<-predict(tmodel, test, "probs")
 predicted_class<-predict(tmodel, test)
 
-#confusion matrix
-table(actual=test$pgm_type_edited, predicted=predicted_class)
+#confusion matrix and accuracy
+library(MLmetrics)
+
+ConfusionMatrix(predicted_class, test$pgm_type_edited)
+Accuracy(predicted_class, test$pgm_type_edited)
