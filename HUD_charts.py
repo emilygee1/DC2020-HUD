18a8f8e12259598d2b38a31a9e370b3802729c9f
Emily Gee <emilygee1@users.noreply.github.com>
2020-02-22 22:12:55 -0500

    Update HUD_charts.py

--- HUD_charts.py
+++ HUD_charts.py
@@ -1,69 +1,47 @@
-
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-df = pd.read_excel('Data_Level2_HUD_HUDPrograms_2009.xlsx')
 
+# Load datasets for 2009, 2014, 2018
+df09 = pd.read_excel('Data_Level2_HUD_HUDPrograms_2009.xlsx')
+df14 = pd.read_excel('Data_Level2_HUD_HUDPrograms_2014.xlsx')
+df18 = pd.read_excel('Data_Level2_HUD_HUDPrograms_2018.xlsx')
 
-race = df['HEAD_RACE_CD'].value_counts(normalize=True) * 100
+df_list = [df09, df14, df18]
 
-print(race)
-plt.pie(race, pctdistance=2, autopct='%1.01f%%')
-plt.legend(labels=race.keys(), bbox_to_anchor=(-0.1, 1.),fontsize=8)
-plt.show()
+# Replace keys for race and program type with the actual names
+for df in df_list:
+    df['HEAD_RACE_CD'] = df['HEAD_RACE_CD'].replace({1:'White',
+                                                    2:'Black',
+                                                    3:'Native American',
+                                                    4:'Asian',
+                                                    5:'Hawaiian or Pacific Islander',
+                                                    6:'More than one race'})
+    df['pgm_type_edited'] = df['pgm_type_edited'].replace({1:'Public housing',
+                                                          2:'Housing voucher',
+                                                          3:'Multi-family'})
 
+year = ['2009', '2014', '2018']
+i = 0
 
+# Pie charts to display program breakdown for each year
+for df in df_list:    
+    race = df['pgm_type_edited'].value_counts(normalize=True) * 100
+    explode = (.1, 0, 0)
+    plt.pie(race, explode = explode, pctdistance=2, autopct='%1.01f%%', shadow=True)
+    plt.title('Breakdown by Program - ' + year[i])
+    plt.legend(labels=race.keys(), bbox_to_anchor=(-0.1, 1.),fontsize=12)
+    plt.show()
+    i += 1
 
-race = df['pgm_type_edited'].value_counts(normalize=True) * 100
+j = 0
 
-print(race)
-plt.pie(race, pctdistance=2, autopct='%1.01f%%')
-plt.legend(labels=race.keys(), bbox_to_anchor=(-0.1, 1.),fontsize=8)
-plt.show()
-
-
-
-df = pd.read_excel('Data_Level2_HUD_HUDPrograms_2014.xlsx')
-
-
-race = df['HEAD_RACE_CD'].value_counts(normalize=True) * 100
-
-print(race)
-plt.pie(race, pctdistance=2, autopct='%1.01f%%')
-plt.legend(labels=race.keys(), bbox_to_anchor=(-0.1, 1.),fontsize=8)
-plt.show()
-
-
-# In[115]:
-
-
-race = df['pgm_type_edited'].value_counts(normalize=True) * 100
-
-print(race)
-plt.pie(race, pctdistance=2, autopct='%1.01f%%')
-plt.legend(labels=race.keys(), bbox_to_anchor=(-0.1, 1.),fontsize=8)
-plt.show()
-
-
-
-
-df = pd.read_excel('Data_Level2_HUD_HUDPrograms_2018.xlsx')
-
-
-
-
-race = df['HEAD_RACE_CD'].value_counts(normalize=True) * 100
-
-print(race)
-plt.pie(race, pctdistance=2, autopct='%1.01f%%')
-plt.legend(labels=race.keys(), bbox_to_anchor=(-0.1, 1.),fontsize=8)
-plt.show()
-
-
-race = df['pgm_type_edited'].value_counts(normalize=True) * 100
-
-print(race)
-plt.pie(race, pctdistance=2, autopct='%1.01f%%')
-plt.legend(labels=race.keys(), bbox_to_anchor=(-0.1, 1.),fontsize=8)
-plt.show()
-
+# Pie charts to display race breakdown of head of households for each year
+for df in df_list:    
+    race = df['HEAD_RACE_CD'].value_counts(normalize=True) * 100
+    explode = (.1, 0, 0, 0, 0, 0)
+    plt.pie(race, explode = explode, pctdistance=2, autopct='%1.01f%%', shadow=True)
+    plt.title('Breakdown by Race - ' + year[j])
+    plt.legend(labels=race.keys(), bbox_to_anchor=(-0.1, 1.),fontsize=12)
+    plt.show()
+    j += 1
